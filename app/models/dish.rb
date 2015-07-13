627f51d7d0f4c328d2e172c9afcbda2b5763e9d8
robinfwzdev <robin@futureworkz.com>
2015-07-13 17:31:01 +0700

    Add testcase for cost field on products

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,4 @@
 class Dish < ActiveRecord::Base
    validates :title, :description, presence: true
+   validates :cost, numericality: { greater_than: 0 }
 end
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Dish, type: :model do
    context 'Validation' do
       it { should validate_presence_of(:title) }
       it { should validate_presence_of(:description) }
+      it { should validate_numericality_of(:cost).is_greater_than(0) }
    end
 end
