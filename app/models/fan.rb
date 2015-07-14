cd21b79949d96a95142184f1bea6f4becd45f2fe
robinfwzdev <robin@futureworkz.com>
2015-07-14 13:26:12 +0700

    Write validates first_name and last_name on fan models

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -1,6 +1,8 @@
 class Fan < ActiveRecord::Base
+   validates :first_name, :last_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 end
