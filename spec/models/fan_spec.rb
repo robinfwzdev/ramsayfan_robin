1e30f4b247beef2ab485e23975d171201d512169
robinfwzdev <robin@futureworkz.com>
2015-07-14 13:25:39 +0700

    Write testcase check validates first_name and last_name on fan

--- spec/models/fan_spec.rb
+++ spec/models/fan_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Fan, type: :model do
   context 'Validation' do
       it { should validate_presence_of(:email) }
       it { should validate_presence_of(:password) }
+      it { should validate_presence_of(:first_name) }
+      it { should validate_presence_of(:last_name) }
   end    
 end
