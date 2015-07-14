d1efc0984508f74a2c6cc7f194c6f2fdcbbdc00e
robinfwzdev <robin@futureworkz.com>
2015-07-14 08:45:59 +0700

    Write testcase show funtion on Controller spec

--- spec/controllers/dishes_controller_spec.rb
+++ spec/controllers/dishes_controller_spec.rb
@@ -4,9 +4,19 @@ RSpec.describe DishesController, type: :controller do
    describe "#index" do
       let!(:dishes){ FactoryGirl.create_list(:dish, 2) }
 
-      it "check list product" do
+      it "check list dish" do
          get :index
          expect(assigns(:dishes).size).to eq dishes.size
       end
    end
+
+   describe '#show' do  
+      let!(:dish){ FactoryGirl.create(:dish) }  
+
+      it 'displays a dish' do
+         get :show, id: dish.id
+         expect(assigns(:dish).id).to eq dish.id
+      end
+   end
+
 end
