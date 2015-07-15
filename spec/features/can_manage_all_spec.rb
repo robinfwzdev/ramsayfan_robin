f1bd106fab50d7406f4deff1679a9658152f7911
robinfwzdev <robin@futureworkz.com>
2015-07-15 09:48:05 +0700

    Write testcase create a dish by fan function on dishes view

--- spec/features/can_manage_all_spec.rb
+++ spec/features/can_manage_all_spec.rb
@@ -14,4 +14,36 @@ RSpec.describe "can manage all spec", :type => :feature do
          expect(page).to have_content 'Pho ga'
       end
    end
+
+   describe '#create' do
+      let!(:fan){ FactoryGirl.create(:fan) }
+
+      context 'User can create a dish' do
+         it 'Create a dish by fan' do
+            visit '/dishes'
+
+            expect(page).to have_link 'Create a dish'
+
+            click_on 'Create a dish'
+
+            fill_in 'Email', with: 'robinhood@example.com'
+            fill_in 'Password', with: '12345678'
+
+            click_on 'Log in'
+
+            expect(page).to have_content 'Create a Dish'
+
+            fill_in 'Title', with: 'Bun thit nuong'
+            fill_in 'Description', with: 'This is very delicious'
+            fill_in 'Cost', with: 10
+            fill_in 'Pax', with: 10
+
+            click_on 'Create Dish'
+            
+            expect(page).to have_content 'Bun thit nuong'            
+
+         end
+      end
+   end
+
 end
