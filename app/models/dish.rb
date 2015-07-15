ddc18d6622faabae17c2ff9752e208fb9939984e
robinfwzdev <robin@futureworkz.com>
2015-07-15 13:10:42 +0700

    Add scope getdishesbyfan on dish models

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -5,4 +5,5 @@ class Dish < ActiveRecord::Base
    validates :cost, :pax, numericality: { greater_than: 0 }
 
    scope :search_keyword, ->(keyword) { where('title ILIKE ?', "#{keyword}%") }
+   scope :getDishesByFan, ->(fan_id) { where('fan_id = ?', fan_id) }
 end
