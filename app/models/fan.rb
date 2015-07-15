663aa1fc95b0823f42453bd2df3ca4eb8b18393a
robinfwzdev <robin@futureworkz.com>
2015-07-15 10:11:23 +0700

    Add has_many :dishes on fan models

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -1,4 +1,6 @@
 class Fan < ActiveRecord::Base
+   has_many :dishes
+   
    validates :first_name, :last_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
