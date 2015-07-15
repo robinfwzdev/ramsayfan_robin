3c246237e1d78eebc7ea83f29f590bc51d78c4d0
robinfwzdev <robin@futureworkz.com>
2015-07-15 10:11:51 +0700

    Add create fan on seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Dish.destroy_all
+Fan.destroy_all
 
-Dish.create(title: 'Pho ga', description: 'This is pho ga', cost: 20, pax: 10, vegetarian: false)
-Dish.create(title: 'Hu tieu', description: 'This is hu tieu nam vang', cost: 30, pax: 10, vegetarian: false)
+fan = Fan.create(first_name: 'robin', last_name: 'futureworkz', email: 'robin@example.com', password: '12345678')
+
+Dish.create(title: 'Pho ga', description: 'This is pho ga', cost: 20, pax: 10, vegetarian: false, fan_id: fan)
+Dish.create(title: 'Hu tieu', description: 'This is hu tieu nam vang', cost: 30, pax: 10, vegetarian: false, fan_id: fan)
